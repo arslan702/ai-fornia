8b2796ac5c47e4c0e6193ed27726a33ec779c181
mycomputer <muhammadarslanjabbar@gmail.com>
2024-02-05 13:41:46 +0500

    firbase-config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoQ6nZbUruhiX5SV37GVsrklVYGS0zJF4",
-  authDomain: "new-ai-c7598.firebaseapp.com",
-  projectId: "new-ai-c7598",
-  storageBucket: "new-ai-c7598.appspot.com",
-  messagingSenderId: "823141649138",
-  appId: "1:823141649138:web:3de282435536327dc49e6b"
+  apiKey: "******",
+  authDomain: "******",
+  projectId: "*******",
+  storageBucket: "*******",
+  messagingSenderId: "********",
+  appId: "*******"
 };
 
 
